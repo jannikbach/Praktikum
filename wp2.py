495f6bdc22c4c75a68e549e166ce21f13409fc55
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2025-01-06 15:41:13 +0100

    get_rc before time measurement

--- wp2.py
+++ wp2.py
@@ -11,17 +11,13 @@ with open('Data/ITS_graphs.pkl.gz', 'rb') as f:
     reactions = pickle.load(f)
 print("Data loaded.")
 
+reactions = list(map(lambda g: get_rc(g['ITS']), reactions))
+
 start_time = time.time()
-clusters = split_by_equality(reactions, is_isomorphic, lambda x: get_rc(x['ITS']))
+clusters = split_by_equality(reactions, is_isomorphic)
 end_time = time.time()
 
-
-print("num reactions:", len(reactions))
-print("num clusters:", len(clusters))
-
-
-print("num clusters:", len(clusters))
-
-# Measure execution time
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time:.2f} seconds")
+print("num reactions:", len(reactions))
+print("num clusters:", len(clusters))
