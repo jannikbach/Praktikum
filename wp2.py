a4cc2cec978e39fb827d0385bab55afd06324dcc
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2025-01-06 13:19:59 +0100

    add matching criteria

--- wp2.py
+++ wp2.py
@@ -5,16 +5,31 @@ import pickle
 import networkx as nx
 from src.rc_extract import get_rc
 
+VERBOSE = False
+
 print("Loading data...")
 with open('Data/ITS_graphs_100_subset.pkl', 'rb') as f:
     reactions = pickle.load(f)
 print("Data loaded.")
 
+
+def node_match(n1, n2):
+    """
+    Check if two nodes should be considered identical
+    """
+    return n1['element'] == n2['element'] and n1['charge'] == n2['charge']
+
+def edge_match(e1, e2):
+    """
+    Check if two edges should be considered identical
+    """
+    return e1['order'] == e2['order']
+
 def is_isomorphic(rc1, rc2):
     """
     Check if two reaction centers are isomorphic.
     """
-    return nx.is_isomorphic(rc1, rc2)
+    return nx.is_isomorphic(rc1, rc2, node_match=node_match, edge_match=edge_match)
 
 """
 each cluster is a tuple (reaction_center, reactions)
@@ -24,7 +39,11 @@ each cluster is a tuple (reaction_center, reactions)
 """
 clusters = []
 
+i = 0
 for reaction in reactions:
+    i+=1
+    if VERBOSE and i % 1000 == 0:
+        print(f"{i}/{len(reactions)}")
     # get reaction center
     reaction_center = get_rc(reaction['ITS'])
 
