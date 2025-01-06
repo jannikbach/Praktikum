9512524229c314fefe3c00c4bd74b0d9d321d3a1
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2025-01-06 14:53:11 +0100

    extract split method

--- split.py
+++ split.py
@@ -4,3 +4,30 @@ from itertools import groupby
 def split_by_key(graphs, key_func):
     graphs.sort(key=key_func)
     return [list(group) for key, group in groupby(graphs, key=key_func)]
+
+
+def split_by_equality(graphs, is_equal, representative_func):
+    if representative_func is None:
+        # use identity
+        representative_func = lambda x: x
+
+    clusters = []
+
+    for graph in graphs:
+        representative = representative_func(graph)
+
+        found_cluster = False
+        for cluster_representative, cluster_elements in clusters:
+            if is_equal(representative, cluster_representative):
+                cluster_elements.append(graph)
+                found_cluster = True
+                break
+
+        if not found_cluster:
+            # spawn new cluster
+            # use representative of the first element as representative of the whole cluster
+            cluster = (representative, [graph])
+            clusters.append(cluster)
+
+    # drop representatives
+    return [elements for representative, elements in clusters]
--- wp2.py
+++ wp2.py
@@ -2,13 +2,14 @@
 Implements a simple clustering algorithm.
 """
 import pickle
+
 import networkx as nx
+
+from split import split_by_equality
 from src.rc_extract import get_rc
 
-VERBOSE = True
-
 print("Loading data...")
-with open('Data/ITS_graphs_100_subset.pkl', 'rb') as f:
+with open('Data/ITS_graphs.pkl.gz', 'rb') as f:
     reactions = pickle.load(f)
 print("Data loaded.")
 
@@ -19,11 +20,13 @@ def node_match(n1, n2):
     """
     return n1['element'] == n2['element'] and n1['charge'] == n2['charge']
 
+
 def edge_match(e1, e2):
     """
     Check if two edges should be considered identical
     """
     return e1['order'] == e2['order']
+
 
 def is_isomorphic(rc1, rc2):
     """
@@ -31,34 +34,8 @@ def is_isomorphic(rc1, rc2):
     """
     return nx.is_isomorphic(rc1, rc2, node_match=node_match, edge_match=edge_match)
 
-"""
-each cluster is a tuple (reaction_center, reactions)
-- reactions: a list of reactions from the original dataset
-- reaction_center: the representative reaction center for this cluster
-  (the reaction center of the first reaction in this list)
-"""
-clusters = []
 
-i = 0
-for reaction in reactions:
-    i+=1
-    if VERBOSE and i % 1000 == 0:
-        print(f"{i}/{len(reactions)}")
-    # get reaction center
-    reaction_center = get_rc(reaction['ITS'])
+clusters = split_by_equality(reactions, is_isomorphic, lambda x: get_rc(x['ITS']))
 
-    # find cluster to add this to
-    found_cluster = False
-    for cluster_reaction_center, cluster_reactions in clusters:
-        if is_isomorphic(reaction_center, cluster_reaction_center):
-            # add to existing cluster
-            cluster_reactions.append(reaction)
-            found_cluster = True
-            break
-
-    if not found_cluster:
-        # spawn new cluster
-        clusters.append((reaction_center, [reaction]))
-
-print("num reactions: ", len(reactions))
+print("num reactions:", len(reactions))
 print("num clusters:", len(clusters))
