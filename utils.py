d194a519ccf436069140d8f7d58698f035f4e17b
jannikbach <jannik.bach@icloud.com>
2025-01-09 13:20:05 +0100

    initial

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import time
 import pickle
+from concurrent.futures import ProcessPoolExecutor
 from itertools import groupby
 from operator import itemgetter
 
@@ -39,17 +40,6 @@ def split_by_equality(cluster, is_equal):
     return buckets
 
 
-def partition_clusters_by_invariant(clusters, invariant):
-    """
-    Partition clusters by invariant
-    """
-    partitioned_clusters = []
-    for cluster in clusters:
-        invariant_sublists = split_by_key(cluster, invariant)
-        partitioned_clusters.extend(invariant_sublists)
-    return partitioned_clusters
-
-
 def node_match(n1, n2):
     """
     Check if two nodes should be considered identical
@@ -72,11 +62,10 @@ def is_isomorphic(rc1, rc2):
 
 
 def get_rc(G: nx.Graph, ID=None) -> nx.Graph:
-
     # Extract edges with "standard_order" not equal to 0
     edges = [(e[0], e[1]) for e in G.edges(data=True) if e[2]["standard_order"] != 0]
     rc = nx.edge_subgraph(G, edges)
-
+    rc = rc.copy()
     # Assign the parent identifier (R-id)
     rc.graph["ID"] = ID
 
@@ -120,7 +109,16 @@ def load_reaction_centers(dataset='small', verbose=True):
     return load_reactioncenters_from_path(graphs_filename, verbose)
 
 
-def run_pipeline(pipeline_title, reaction_centers, steps, iso=True):
+def _unflatten(graphs, cluster_sizes):
+    graph_i = 0
+    clusters = []
+    for cluster_size in cluster_sizes:
+        clusters.append(graphs[graph_i:graph_i + cluster_size])
+        graph_i += cluster_size
+    return clusters
+
+
+def run_pipeline(pipeline_title, reaction_centers, steps, max_workers=1, iso=True):
     clusters = [reaction_centers]
     print(f"===== '{pipeline_title}' =====")
 
@@ -135,12 +133,34 @@ def run_pipeline(pipeline_title, reaction_centers, steps, iso=True):
     total_start_time = time.time()
     for transformation, invariant in steps:
         start_time = time.time()
-        if transformation is not None:
-            for cluster in clusters:
-                for graph in cluster:
-                    # in-place transformation
-                    transformation(graph)
-        clusters = partition_clusters_by_invariant(clusters, invariant)
+
+        cluster_sizes = [len(cluster) for cluster in clusters]
+
+        # flatten
+        all_graphs = list(more_itertools.flatten(clusters))
+
+        # transform parallel
+        with ProcessPoolExecutor(max_workers=max_workers) as executor:
+            if transformation is not None:
+                all_graphs = list(executor.map(transformation, all_graphs))
+            labels = list(executor.map(invariant, all_graphs))
+        decorated_graphs = list(zip(labels, all_graphs))
+
+        # unflatten
+        clusters_of_decorated_graphs = _unflatten(decorated_graphs, cluster_sizes)
+
+        clusters = []
+        for cluster_of_decorated_graphs in clusters_of_decorated_graphs:
+            # Step 2: Sort the decorated list by key
+            cluster_of_decorated_graphs.sort(key=itemgetter(0))
+
+            # Step 3: Group by the precomputed key
+            grouped = groupby(cluster_of_decorated_graphs, key=itemgetter(0))
+
+            for _, group in grouped:
+                cluster_of_undecorated_graphs = [item for _, item in group]
+                clusters.append(cluster_of_undecorated_graphs)
+
         end_time = time.time()
 
         if transformation is None:
--- weisfeiler_lehmann.py
+++ weisfeiler_lehmann.py
@@ -3,16 +3,16 @@ from xxhash import xxh64
 
 
 # invariant
-def wl_init(graph) -> None:
-    weisfeiler_lehmann_init(graph, node_attr=["element", "charge"], edge_attr=["order"])
+def wl_init(graph) -> nx.Graph:
+    return weisfeiler_lehmann_init(graph, node_attr=["element", "charge"], edge_attr=["order"])
 
 
 # invariant
-def wl_step(graph) -> None:
-    weisfeiler_lehmann_step(graph)
+def wl_step(graph) -> nx.Graph:
+    return weisfeiler_lehmann_step(graph)
 
 
-def weisfeiler_lehmann_init(graph: nx.Graph, node_attr: list[str], edge_attr: list[str]):
+def weisfeiler_lehmann_init(graph: nx.Graph, node_attr: list[str], edge_attr: list[str]) -> nx.Graph:
     """
     The first iteration step that uses node/edge attributes to create hashes.
     Adds the "hash" attribute to each node.
@@ -35,8 +35,10 @@ def weisfeiler_lehmann_init(graph: nx.Graph, node_attr: list[str], edge_attr: li
     for node, node_hash in new_hashes.items():
         graph.nodes[node]["hash"] = node_hash
 
+    return graph
 
-def weisfeiler_lehmann_step(graph: nx.Graph):
+
+def weisfeiler_lehmann_step(graph: nx.Graph) -> nx.Graph:
     new_hashes = {}  # store hashes for the next iteration (don't override the current ones)
 
     for node, node_data in graph.nodes(data=True):
@@ -55,6 +57,8 @@ def weisfeiler_lehmann_step(graph: nx.Graph):
     for node, new_hash in new_hashes.items():
         graph.nodes[node]["hash"] = new_hash
 
+    return graph
+
 
 def hash_graph(graph: nx.Graph) -> int:
     d = {}
@@ -68,11 +72,11 @@ def hash_graph(graph: nx.Graph) -> int:
 
 
 def _node_label(node, node_attrs):
-    return ",".join(sorted([str(node[a]) for a in node_attrs]))
+    return ",".join([str(node[a]) for a in node_attrs])
 
 
 def _edge_label(edge, edge_attrs):
-    return ",".join(sorted([str(edge[a]) for a in edge_attrs]))
+    return ",".join([str(edge[a]) for a in edge_attrs])
 
 
 def _get_initial_neighbor_label(graph: nx.Graph, from_node, to_node, node_attr, edge_attr) -> str:
